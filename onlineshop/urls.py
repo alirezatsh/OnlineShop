f269fb3bc53cfba4bdf401210183690f10550433
alireza <alirezatsh1383@gmail.com>
2024-11-14 12:55:16 +0330

    initialize the project and add schema

---
+++ onlineshop/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path , include
+from rest_framework.schemas import get_schema_view
+from drf_spectacular.views import SpectacularSwaggerView , SpectacularAPIView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('schema/' , SpectacularAPIView.as_view() , name='schema'),
+    path('schema/swagger-ui/' , SpectacularSwaggerView.as_view(url_name='schema') , name='swagger-ui'),
+    path(
+        "schema/",
+        get_schema_view(
+            title="OnlineShop project", description="API for all things …", version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
+]
+
