bc46925aacdc2b91e0173573578637d633614d35
Legimmort82 <legendlegendery1212@gmail.com>
2024-11-27 15:52:14 +0330

    fixed

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -156,11 +156,13 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8000",
-#     "http://localhost:3000", 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:3000", 
 
-# ]
+]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 # CORS_ALLOW_HEADERS = [
 #     'authorization',
