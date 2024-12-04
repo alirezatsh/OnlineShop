46657b75f49f86ab12c2183c911ce75212274c4d
alireza <alirezatsh1383@gmail.com>
2024-12-04 13:07:03 +0330

    remove additional files

--- products/serializers.py
+++ products/serializers.py
@@ -24,6 +24,7 @@ class TurnIdIntoString(serializers.ModelSerializer):
             representation['review'] = None
         return representation
 
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
